d65b38f6edf28a1fcac6115c61690b8c05da6be4
Girish <girishby9@gmail.com>
2017-07-03 15:30:30 +0530

    share commit

--- app/src/main/java/example/com/abhi/gittestone/MainActivity.java
+++ app/src/main/java/example/com/abhi/gittestone/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("TEST","hjhh");
+
+        Log.i("TEST","hjhhghbotrigkjbih");
    
     }
 }
